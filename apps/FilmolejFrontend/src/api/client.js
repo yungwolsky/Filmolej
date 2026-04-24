2bf6d687faaa85c7897d346c9f24773063b371e0
yungwolsky <mikolajwolskinauka@gmail.com>
2026-04-24 11:33:50 +0200

    Fix in client.js

--- apps/FilmolejFrontend/src/api/client.js
+++ apps/FilmolejFrontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-   baseURL: "http:/localhost:5000/api",
+   baseURL: "http://localhost:5000/api",
 });
 
 apiClient.interceptors.request.use((config) => {
